a5f5c7cfc0407304e23de18df128f38cff01f229
shevchukm <mmikrob@gmail.com>
2018-06-25 16:56:41 +0300

    add tests for goods controller

--- auth/test/test.js
+++ auth/test/test.js
@@ -1,19 +1,58 @@
 const expect = require('chai').expect;
 const sinon  = require('sinon');
+const UsersModel = require('../../users/UsersModel')
 const AuthController = require('../AuthController')
 
-describe('Class AuthController', () => {
+describe('AuthController', () => {
+    mockUsersModel = sinon.mock(UsersModel);
+    let req = {
+        headers: {token: ''},
+        body: {}
+    };
+    let res = {
+        data: {},
+        status: function(data){this.data.status = data; return this},
+        send: function(data){this.data.send = data}
+    };
+    const next = () => {}
+
     describe('#authorization', () => {
         it("should return TypeError without arguments", () => {
             expect(AuthController.authorization).to.throw(TypeError);
+            
         });
 
         it("should accept three arguments", () => {
             expect(AuthController.authorization.length).to.equal(3);
         });
         
-        it("should call response with error", () => {
+        it('should send error with status 403 and message "you are not logged in"', ()=> {
+            AuthController.authorization(req, res, next);
+            expect(res.data).to.deep.equal({status: 403, send: 'you are not logged in '});
+        });
 
+        it('should set _id to request body if token pass verify', ()=> {
+            req.headers.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjViMjdiYzNiNDk4ZDkzMzk1ZjNiYWFjZiIsIm5hbWUiOiJNeWtoYWlsbyBTaGV2Y2h1ayIsImVtYWlsIjoibW1pa3JvYkBnbWFpbC5jb20iLCJwYXNzd29yZCI6IjM2OTg1MiIsImNvbmZpcm0iOnRydWUsImdvb2RzIjpbeyJfaWQiOiI1YjJhNWQ1Mjc3ZTY2YzI1NjE2ZTk1ZGEiLCJjYXRlZ29yeSI6ImNhcnMiLCJwcmljZSI6IjgwMCIsInN0b2NrZWQiOnRydWUsIm5hbWUiOiJwb3JzaGUifV19LCJpYXQiOjE1Mjk5MzM1NTB9.JnDxQRdhRyob6QUjIEmr-qPtiLDkru44e9PnUmQjrus'
+            AuthController.authorization(req, res, next);
+            expect(req.body._id).to.equal('5b27bc3b498d93395f3baacf');
+        });
+    });
+
+    describe('#login', () => {
+        it('should send error with status 500 and error message', () => {
+            req.body.user = {email: ''};
+            const error = true
+            mockUsersModel.expects('getOneUser').yields(error, {name: 'misha', email: ''});
+            AuthController.login(req, res);
+            expect(res.data).to.be.deep.equal({status: 500, send: true})
+        });
+
+        it('should send error with status 404 and error message "user not found"', () => {
+            req.body.user = {email: ''};
+            const error = null;
+            mockUsersModel.expects('getOneUser').yields(error, null);
+            AuthController.login(req, res);
+            expect(res.data).to.be.deep.equal({status: 404, send: "user not found"})
         });
     });
 });
---
+++ goods/test/_testGoodsController.js
@@ -0,0 +1,113 @@
+const expect = require('chai').expect;
+const sinon  = require('sinon');
+const GoodsModel = require('../GoodsModel');
+const GoodsController = require('../GoodsController');
+
+describe("GoodsController", function() {
+    const GoodsModelMock = sinon.mock(GoodsModel);
+    let req = {
+        params: {id: 1},
+        body: {id: 1}
+    };
+    let res = {
+        data: {},
+        status: function(data){this.data.status = data; return this},
+        send: function(data){this.data.send = data}
+    };
+
+    describe("#getAllGoods", ()=>{
+        it("should send all goods", done => {
+            const expectedResult = { goods: [ {a:1}, {b:2}] };
+            GoodsModelMock.expects('getAllGoods').yields(null, expectedResult);    
+            GoodsController.getAllGoods(req,res);
+            expect(res.data.send.goods).to.deep.equal([ {a:1}, {b:2} ]);
+            done()
+        });
+    
+        it("should send an error 'something went wrong on loading goods' with status 500", done => {
+            GoodsModelMock.expects('getAllGoods').yields(true, []);
+            GoodsController.getAllGoods(req, res);
+            expect(res.data).to.deep.equal({status: 500, send: 'something went wrong on loading goods'});
+            done()
+        });
+    });
+    describe("#getOneGood", () => {
+        it("should send one good", done => {
+            const expectedResult = {_id: 1 ,name: 'mike', age: 22};
+            GoodsModelMock.expects('getOneGood').yields(null, expectedResult)    
+            GoodsController.getOneGood(req, res);
+            expect(res.data.send).to.deep.equal({_id: 1 ,name: 'mike', age: 22});
+            done()
+        });
+    
+        it("should send an error 'something went wrong on loading this good' with status 500", done => {
+            const expectedResult = {_id: 1 ,name: 'mike', age: 22};
+            GoodsModelMock.expects('getOneGood').yields(true, expectedResult);
+            GoodsController.getOneGood(req, res);
+            expect(res.data).to.deep.equal({status: 500, send: 'something went wrong on loading this good'});
+            done()
+        });
+    });
+
+    describe("#createGood", () => {
+        it("should send status 200 and message 'successfully added'", done => {
+            GoodsModelMock.expects('createGood').yields(null)    
+            GoodsController.createGood(req, res);
+            expect(res.data).to.deep.equal({status: 200, send: 'successfully added'});
+            done()
+        });
+    
+        it("should send an error 'server have got problems with add good' with status 500 ", done => {
+
+            GoodsModelMock.expects('createGood').yields(true);
+            GoodsController.createGood(req, res);
+            expect(res.data).to.deep.equal({status: 500, send: 'server have got problems with add good'});
+            done()
+        });
+    });
+
+    describe("#deleteGood", () => {
+        it("should send status 200 and message 'successfully deleted'", done => {
+            GoodsModelMock.expects('deleteGood').yields(null);
+            GoodsController.deleteGood(req, res);
+            expect(res.data).to.deep.equal({status: 200, send: 'successfully deleted'});
+            done()
+        });
+    
+        it("should send an error 'server have got problems with delete good' with status 500 ", done => {
+            GoodsModelMock.expects('deleteGood').yields(true);
+            GoodsController.deleteGood(req, res);
+            expect(res.data).to.deep.equal({status: 500, send: 'server have got problems with delete good'});
+            done()
+        });
+    });
+
+    describe("#responseHandler", () => {
+        it("should send status 200 and message 'it success message'", done => {
+
+            const messages = [ 'it error message', 'it success message']
+
+            GoodsController.responseHandler(null, res, messages);
+            expect(res.data).to.deep.equal({status: 200, send: 'it success message'});
+            done()
+        });
+
+        it("should send status 500 and message 'it error message'", done => {
+            messages = [ 'it error message', 'it success message'];
+            GoodsController.responseHandler(true, res, messages);
+            expect(res.data).to.deep.equal({status: 500, send: 'it error message'});
+            done()
+        });
+
+        it("should send object", done => {
+
+            const expectedResult = { goods: [ {a:1}, {b:2}] };
+
+            const messages = [ 'it error message', 'it success message'];
+
+            GoodsController.responseHandler(null, res, messages, expectedResult );
+            expect(res.data.send).to.deep.equal(expectedResult);
+            done()
+        });
+    });
+});
